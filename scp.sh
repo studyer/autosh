f8d20267ca40e6b66c7965b17efb2cfde503bdbe
studyer <studyer@qq.com>
2015-08-17 00:11:08 +0800

    fix bug for scp shell

--- scp.sh
+++ scp.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/expect -f
 
 set timeout 60
-set flag [lindex $argv 0]
-set src_file [lindex $argv 1]
-set dest_file [lindex $argv 2]
-# s2l server to local
-if { $flag == "s2l" } {
+
+if { $argc > 2 } {
+    set flag [lindex $argv 0]
+    set src_file [lindex $argv 1]
+    set dest_file [lindex $argv 2]
     spawn scp username@host:$src_file $dest_file
 } else {
+    set src_file [lindex $argv 0]
+    set dest_file [lindex $argv 1]
     spawn scp $src_file username@host:$dest_file
 }
 expect "*assword:" 
